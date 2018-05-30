0b4182ebd96e65a58cc2bf68ee6ce0dc03bcab70
Administrator <397938635@qq.com>
2018-05-30 17:47:22 +0800

    0530

--- src/component/userManage/Registration.js
+++ src/component/userManage/Registration.js
@@ -1,49 +1,72 @@
 import React, {Component} from 'react';
 import {List, InputItem, WhiteSpace, Button, Toast, WingBlank, Radio} from 'antd-mobile';
 import {Link} from 'react-router-dom';
+import { createForm, formShape } from 'rc-form';
+import propTypes from 'prop-types';
 import imgURL from './../img/logo.png';
 
 const RadioItem = Radio.RadioItem;
-export default class Registration extends Component {
+export  class Registration extends Component {
+   static propTypes={form:formShape}
     constructor(props) {
         super(props);
         this.state = {
             hasError: false,
-            username: '',
-            phone: '',
+            'username': '',
+            'phone': '',
             password: '',
             confirmPassword: '',
             type:'genius'
         }
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleTypeChange=this.handleTypeChange.bind(this);
+
+        this.handleValueChange=this.handleValueChange.bind(this);
     }
 
-    handlePasswordChange(e) {
+   submit=()=>{
+       this.props.form.validateFields((error,value)=>{
+        setTimeout(()=>{
+            if(value.user==='yiminghe'){
+                this.props.form.setFields({
+                    user:{value:values.user,
+                    errors:[new Error('forbid ha')]}
+                })
+            }
+        })
+       })
+   }
+    handleValueChange(key,value){
 
-    }
-    handleTypeChange(value){
-        this.setState({type:value});
+        this.setState({[key]:value});
     }
     render() {
         const {type}=this.state
+      const {getFieldProps,getFieldError}=this.props.form;
+        const usernameError=getFieldError('username');
+        console.log(usernameError);
         return (<List>
             <WingBlank>
                 <div style={{textAlign: 'center'}}><img className='logo' src={imgURL}/></div>
             </WingBlank>
-            <WingBlank> <InputItem>用户名： </InputItem></WingBlank>
+            <WingBlank> <InputItem {...getFieldProps('username',{
+                onChange(){(value)=>this.handleValueChange('username',value)},
+                rules:[{required:true,
+                message:'用户名必填',
+                }]
+            })}>用户名：</InputItem></WingBlank>
+            <p className='validate'> {usernameError?usernameError:null}</p>
             <WhiteSpace/>
-            <WingBlank> <InputItem>注册： </InputItem></WingBlank>
             <WhiteSpace/>
-            <WingBlank> <InputItem onChange={this.handlePasswordChange}>密码： </InputItem></WingBlank>
+            <WingBlank> <InputItem onChange={(value)=>this.handleValueChange('password',value)}>密码： </InputItem></WingBlank>
             <WhiteSpace/>
-            <WingBlank> <InputItem>确认密码：</InputItem></WingBlank>
-            <WingBlank> <RadioItem key={0} checked={type==='genius'} onChange={() => this.handleTypeChange('genius')}>
+            <WingBlank> <InputItem onChange={(value)=>this.handleValueChange('password',value)}>确认密码：</InputItem></WingBlank>
+            <WingBlank> <RadioItem key={0} checked={type==='genius'} onChange={(value) => this.handleValueChange('type','genius')}>
                 Genius
             </RadioItem> </WingBlank>
-            <WingBlank><RadioItem key={1} checked={type==='StreetGirl'} onChange={()=>this.handleTypeChange('StreetGirl') }> Street Girl</RadioItem> </WingBlank>
-            <WingBlank> <Button><Link to={'/login'}>Login</Link></Button></WingBlank>
-            <WingBlank> <Button><Link to={'/registration'}>Register</Link></Button></WingBlank>
+            <WingBlank><RadioItem key={1} checked={type==='StreetGirl'} onChange={(value)=>this.handleValueChange('type','StreetGirl') }> Street Girl</RadioItem> </WingBlank>
+            <WingBlank> <Button type={'primary'}><Link to={'/login'}>Login</Link></Button></WingBlank>
+            <WhiteSpace/>
+            <WingBlank> <Button type={'primary'}>Register</Button></WingBlank>
         </List>)
     }
 }
+export default  createForm()(Registration);
