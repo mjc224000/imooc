b4186198b8cd0d6b971b95224d33e480688ffeb1
39793 <397938635@qq.com>
2018-06-04 00:43:21 +0800

    529

--- src/component/Boss/Boss.js
+++ src/component/Boss/Boss.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Icon, InputItem, List, NavBar, WhiteSpace,TextareaItem,WingBlank,Button,Modal} from 'antd-mobile';
+import {Icon, InputItem, List, NavBar, WhiteSpace,TextareaItem,WingBlank,Button,Modal,Grid} from 'antd-mobile';
 import {Link} from 'react-router-dom'
 import imgurl from  "./../img/logo.png"
 export default class Boss extends Component {
@@ -21,8 +21,24 @@ export default class Boss extends Component {
      handleSubmit(){
         this.props.onSubmit(this.state);
      }
+     _avatars(){
+        let ImgList=[];
+         if (this.props.avatars) {
+             ImgList=this.props.avatars;
+         }else {
+             for(var i=1;i<=5;i++){
+                 ImgList.push('avatars-material-man-'+i+'.png');
+                 ImgList.push('avatars-material-woman-'+i+'.png');
+             }
+           ImgList=ImgList.map(v=>{
+               return {icon:require('./../img/'+v)}
+           });
+         }
+       return ImgList
+     }
     render() {
             const ListItem=List.Item;
+            this._avatars();
         return (<div>
             <NavBar leftContent={<Icon type={'left'} onClick={() => this.props.history.go(-1)}/>}>
                 Complete information
@@ -31,12 +47,17 @@ export default class Boss extends Component {
                 <WingBlank><Modal visible={this.state.showModal}
                     transparent
                                   wrapProps={{ onTouchStart: ()=>this.setState({'showModal':false})}}>
-                    <List>
-                        <ListItem>1</ListItem>
-                        <ListItem>1</ListItem>
-                        <ListItem>1</ListItem>
-                        <ListItem>1</ListItem>
-                    </List>
+                   <Grid data={this._avatars()}
+                   columnNum={3}
+                         renderItem={dataItem => (
+                             <div style={{border:"1px solid sliver"}}>
+                                 <img src={dataItem.icon} style={{ width: '75px', height: '75px' }} alt="" />
+                                 <div style={{ color: '#888', fontSize: '14px', marginTop: '12px' }}>
+                                     <span>I am title..</span>
+                                 </div>
+                             </div>
+                         )}
+                   />
                 </Modal> </WingBlank>
                 <WingBlank> <InputItem
                     onChange={(value) => this.handleValueChange('position', value)}>职位招聘</InputItem></WingBlank>
