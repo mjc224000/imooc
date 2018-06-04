1e26b80b1b53ef952c211d038c506d309cbf9bd6
Administrator <397938635@qq.com>
2018-06-04 11:44:25 +0800

    0604

--- src/component/Boss/Boss.js
+++ src/component/Boss/Boss.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
-import {Icon, InputItem, List, NavBar, WhiteSpace,TextareaItem,WingBlank,Button,Modal} from 'antd-mobile';
-import {Link} from 'react-router-dom'
+import {Icon, InputItem, List, NavBar, WhiteSpace,TextareaItem,WingBlank,Button,Modal,Grid} from 'antd-mobile';
+import {Link} from 'react-router-dom';
+import propTypes from 'prop-types';
 import imgurl from  "./../img/logo.png"
 export default class Boss extends Component {
+    static propTypes={
+        update:propTypes.func
+    }
     constructor(props) {
         super(props)
         this.state = {
@@ -10,19 +14,44 @@ export default class Boss extends Component {
             position: null,
             positionDesc: null,
             salary: 0,
-            showModal:true
+            showModal:true,
+            avatar:''
         }
-        this.handleValueChange = this.handleValueChange.bind(this)
+        this.handleValueChange = this.handleValueChange.bind(this);
+        this.handleAvatarClick=this.handleAvatarClick.bind(this);
+        this.handleModalTouch=this.handleModalTouch.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
     }
 
     handleValueChange(key, value) {
         this.setState({[key]: value});
     }
+    handleModalTouch(e){
+       this.setState({avatar:e.icon})
+    }
+    handleAvatarClick(){
+        this.setState({showModal:true })
+    }
      handleSubmit(){
-        this.props.onSubmit(this.state);
+ const{company,position,positionDesc,salary,avatar}=this.state;
+     this.props.update({company,position,positionDesc,salary,avatar});
+     }
+     _avartars(){
+        let avatarList=[];
+        if(this.props.avatars){
+            avatarList=this.props.avatars
+        }else{
+            for(var i=1;i<5;i++){
+                avatarList.push('avatars-material-man-'+i+'.png');
+                avatarList.push('avatars-material-woman-'+i+'.png');
+            }
+            avatarList=avatarList.map((v)=> {
+                return{icon:require('./../img/'+v) }
+            })
+        }
+        return avatarList;
      }
     render() {
-            const ListItem=List.Item;
         return (<div>
             <NavBar leftContent={<Icon type={'left'} onClick={() => this.props.history.go(-1)}/>}>
                 Complete information
@@ -31,23 +60,37 @@ export default class Boss extends Component {
                 <WingBlank><Modal visible={this.state.showModal}
                     transparent
                                   wrapProps={{ onTouchStart: ()=>this.setState({'showModal':false})}}>
-                    <List>
-                        <ListItem>1</ListItem>
-                        <ListItem>1</ListItem>
-                        <ListItem>1</ListItem>
-                        <ListItem>1</ListItem>
-                    </List>
+            <Grid data={this._avartars()}
+                  columnNum={3}
+                 onClick={this.handleModalTouch}
+                  renderItem={dataItem => (
+                      <div style={{border:'1px solid sliver'}}>
+                          <img src={dataItem.icon} style={{ width: '75px', height: '75px' }} alt="" />
+                          <div style={{ color: '#888', fontSize: '14px', marginTop: '12px' }}>
+                              <span>I am title..</span>
+                          </div>
+                      </div>
+                  )}
+            />
                 </Modal> </WingBlank>
+                <WingBlank ><div className={'user-avatar'}>
+                    <div className='user-avatar-user' >
+                     <div className='username'>  <span> imooc</span> </div>
+                        <div className='info'><Link to={'/companyInfo'}>  <span>company info </span></Link></div>
+                    </div>
+                    <div className='user-avatar-avatar' onClick={this.handleAvatarClick} > <img src={this._avartars()[0].icon}/></div>
+                </div></WingBlank>
                 <WingBlank> <InputItem
-                    onChange={(value) => this.handleValueChange('position', value)}>职位招聘</InputItem></WingBlank>
+                    onChange={(value) => this.handleValueChange('position', value)}>职位招聘：</InputItem></WingBlank>
                 <WingBlank> <InputItem
-                    onChange={(value) => this.handleValueChange('company', value)}>公司名称</InputItem></WingBlank>
+                    onChange={(value) => this.handleValueChange('company', value)}>公司名称：</InputItem></WingBlank>
                 <WingBlank> <InputItem
-                    onChange={(value) => this.handleValueChange('salary', value)}>职位薪资</InputItem></WingBlank>
+                    onChange={(value) => this.handleValueChange('salary', value)}>职位薪资：</InputItem></WingBlank>
                 <WhiteSpace/>
-                <WingBlank><TextareaItem title={'职位描述'}
+                <WingBlank><TextareaItem title={'职位描述：'}
+                                         autoHeight
                                          row={3} onChange={(value)=>this.handleValueChange('positionDesc',value)}/> </WingBlank>
-                <WingBlank> <Button type={'primary'}>提交</Button></WingBlank>
+                <WingBlank> <Button onClick={this.handleSubmit} type={'primary'}>提交</Button></WingBlank>
             </List>
         </div>)
     }
--- src/reducer/AuthReducer.js
+++ src/reducer/AuthReducer.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import {getRedirectPath} from './util';
 
-const LOGIN = 'LOGIN';
-const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+
+
 const LOGIN_ERROR = 'LOGIN_ERROR';
 const LOGOUT = 'LOGOUT';
-const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 const REGISTER_ERROR = 'REGISTER_ERROR';
+const AUTH_SUCCESS='AUTH_SUCCESS';
 const Auth = {
     auth: false,
     loading: false,
@@ -16,16 +16,11 @@ const Auth = {
 //reducer
 export default function AuthReducer(state = Auth, action) {
     switch (action.type) {
-        case LOGIN:
-            return {...state, auth: true};
-        case LOGIN_SUCCESS:
-            return {...state, auth: true}
+        case AUTH_SUCCESS: return{...state,auth:true}
         case LOGIN_ERROR:
             return {...state, auth: false, loginErrMsg: action.errMsg}
         case LOGOUT:
             return {...state, auth: false};
-        case REGISTER_SUCCESS:
-            return {...state, auth: true, registerError: null};
         case REGISTER_ERROR:
             return {...state, auth: false, registerErrMsg: action.errMsg};
         default :
@@ -37,28 +32,24 @@ export default function AuthReducer(state = Auth, action) {
 export function login({username, password, cb}) {
     if (!username.length || !password.length) {
         cb();
-        console.log(username, password);
         return {type: LOGIN_ERROR, errMsg: 'username and password can not be empty'}
     }
     return function (dispatch) {
         axios.post('/user/login', {username, password}).then(function (res) {
-            console.log(res.data);
             if (res.status === 200 && res.data.code === 0) {
                 const {type,avatar} = res.data.data;
-                console.log(res.data,type);
                 cb(getRedirectPath({type,avatar}));
-                return dispatch(loginSuccess())
+                return dispatch(authSuccess())
             } else {
-                cb()
+                cb();
                 return dispatch(loginErr('网络错误'))
             }
 
         })
     }
 }
-
-function loginSuccess() {
-    return {type: LOGIN_SUCCESS}
+function authSuccess() {
+    return {type:AUTH_SUCCESS}
 }
 
 function loginErr(errMsg) {
@@ -71,11 +62,6 @@ export function logout() {
 function registerError(msg) {
     return {type: REGISTER_ERROR, errMsg: msg}
 }
-
-function registerSuccess() {
-    return {type: REGISTER_SUCCESS}
-}
-
 export function register(option) {
     const {username, repeatPassword, password, type, cb} = option;
     if (username.replace(' ', '').length !== username.length) {
@@ -86,18 +72,17 @@ export function register(option) {
         cb();
         return registerError('username and password can not be empty');
     }
-    if (repeatPassword != password) {
+    if (repeatPassword !== password) {
         cb();
         return registerError('the both input password must be consistent');
     }
     return function (dispatch) {
         axios.post('/user/register', {...option}).then(res => {
 
-            if (res.status == 200 && res.data.code === 0) {
-                console.log(res.data);
-                console.log(type);
+            if (res.status === 200 && res.data.code === 0) {
+             
                 cb(getRedirectPath({type,avatar:null}))
-                dispatch(registerSuccess());
+                dispatch(authSuccess());
             }
         });
     }
