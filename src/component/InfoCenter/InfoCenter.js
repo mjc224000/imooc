87983a288d7b347332d02658bef1033e75647f33
Administrator <397938635@qq.com>
2018-06-05 17:29:55 +0800

    0605

--- src/component/InfoCenter/InfoCenter.js
+++ src/component/InfoCenter/InfoCenter.js
@@ -1,6 +1,16 @@
 import React,{Component} from 'react';
 export default class InfoCenter extends Component{
+    constructor(props){
+        super(props);
+        this.state={myIndex:0}
+    }
+    handleKeyDown(){
+        const {myIndex}=this.state;
+        this.setState({myIndex:myIndex+1});
+    }
     render(){
-      return(<h2> info center </h2>   )
+      return(<div onKeyDown={this.handleKeyDown.bind(this)}>
+          <input type="text" autoFocus={this.state.myIndex===1}/>
+      </div>)
     }
 }
