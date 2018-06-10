90e1fb0725b748910b7921bd5ff99e2f56d408af
39793 <397938635@qq.com>
2018-06-10 09:50:18 +0800

    Merge branch 'master' of https://github.com/mjc224000/imooc
                                  # Conflicts:
                                  #	src/component/Boss/Boss.js

---
+++ src/container/BossList/BossList.js
@@ -0,0 +1,28 @@
+import BossList from './../../component/BossList/BossList';
+import {connect} from 'react-redux';
+import React,{Component} from 'react';
+import {_axios} from "../../reducer/util";
+const mapStateToProps=({AuthReducer:state})=>{
+    return{...state}
+}
+
+ class BossListContainer extends Com{
+    constructor(props){
+        super(props);
+        this.state={bossList:[]};
+    }
+    componentDidMount(){
+        _axios.get('/info/List',{params:{
+            type:'boss'
+            }}).then((res)=>{
+                if(res.status==200&&res.data.code==0){
+                    this.setState({bossList:res.data.data})
+                }
+        })
+    }
+
+    render(){
+        return<BossList {...this.props} bossList={this.state.bossList} />
+    }
+}
+export default connect(mapStateToProps)(BossListContainer);
